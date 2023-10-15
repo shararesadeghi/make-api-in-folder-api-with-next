7499760d5ecf893da8d52878febec759a7307d10
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-15 17:17:34 +0330

    handle get,post& delete in api

---
+++ project/pages/api/todos/index.js
@@ -0,0 +1,25 @@
+import { todos } from "../../../data/todos";
+
+
+export default function handler(req,res){
+    if(req.method === "GET"){
+        res.status(200).json(todos)
+    }else if(req.method === "POST"){
+        const {todo} = req.body;
+        //Store data in a DB
+        const newTodo = {
+            id: todos.length +1,
+            title:todo,
+        };
+        res.status(201).json({
+            message: "Success",
+            data: newTodo
+        })
+    }else if(req.method === "DELETE"){
+        //Delete all todos
+        res.status(200).json({
+            message: "All todos deleted",
+            data:[]
+        })
+    }
+}
--- project/pages/index.js
+++ project/pages/index.js
@@ -1,11 +1,53 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import {useEffect, useState} from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+      const res = await fetch("/api/todos");
+      const data = await res.json();
+      setTodos(data);
+    }
+    fetchData();
+  },[])
+
+  const clickHandler = async ()=>{
+    const res = await fetch("/api/todos",{
+      method: "POST",
+      body: JSON.stringify({todo}),
+      headers: {"Content-Type":"application/json"}
+    });
+
+    const data = await res.json();
+    console.log(data);
+  }
+
+  const deleteHandler = async ()=>{
+    const res = await fetch("/api/todos",{
+      method:"DELETE"
+    });
+    const data = await res.json();
+    setTodos(data.data);
+
+  }
   return (
     <div className={styles.container}>
       <h1>Api Routes Home page</h1>
+      <ul>
+        {todos.map(todo=>
+          (<li key={todo.id}>{todo.title}</li>))}
+      </ul>
+      <div>
+        <input value={todo} onChange={e=>setTodo(e.target.value)}/>
+        <button onClick={clickHandler}>Create Todo</button>
+      </div>
+      <div>
+        <button onClick={deleteHandler}>Delete All</button>
+      </div>
     </div>
   )
 }
